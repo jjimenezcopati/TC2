27e867e247eb977d432ca030f017b97cc5ba9ca2
jjimenezcopati <jjimenezcopati>
2023-05-23 23:23:52 -0300

    subo programas de python importantes

--- python/segundo_python.py
+++ python/segundo_python.py
@@ -6,43 +6,60 @@
 """
 
 #%% Librerías externas NumPy, SciPy y Matplotlib
-from scipy.signal import TransferFunction
+import scipy.signal as sig                      #TransferFunction zpf2tf
 import matplotlib.pyplot as plt
 import numpy as np
-
+import math as m
 
 ##### Librería de TC2, esta la vas a usar mucho
 from pytc2.sistemas_lineales import pzmap, GroupDelay, bodePlot
 
+#   Defino unos parámetros posibles para 2 tf
 
-w0 = 1
-qq = np.sqrt(2)/2
+#TF1
+phi1=m.pi / 4
+qq1 = 2*m.cos(phi1)
+w01 = 1
+
+#TF2
+phi2=m.pi / 8
+qq2 = 2*m.cos(phi2)
+w02 = 1
 
 #%% FORMA NO ITERATIVA
 
-my_tf = TransferFunction( [w0**2], [1, w0/qq, w0**2] )
-my_tf2 = TransferFunction( [w0**2], [1, w0/(qq*2), w0**2] )
 
+#obtengo la tf a partir de numerador y denominador
+my_tf1 = sig.TransferFunction( [w01**2], [1, w01/qq1, w01**2] )
+my_tf2 = sig.TransferFunction( [w02**2], [1, w02/qq2, w02**2] )
+
+#borra datos anteriores de los plots
 plt.close('all')
 
-bodePlot(my_tf, fig_id=1, filter_description = 'Q={:3.3f}'.format(qq) ) #bode tf 1
-bodePlot(my_tf2, fig_id=1, filter_description = 'Q={:3.3f}'.format(qq*2) ) #bode tf2
+#bode
+bodePlot(my_tf1, fig_id=1, filter_description = 'Q={:3.3f}'.format(qq1) ) #bode tf 1
+bodePlot(my_tf2, fig_id=1, filter_description = 'Q={:3.3f}'.format(qq2) ) #bode tf2
 
-pzmap(my_tf, fig_id=2, filter_description = 'Q={:3.3f}'.format(qq) ) #S plane pole/zero plot tf 1
+#pol y ceros
+pzmap(my_tf1, fig_id=2, filter_description = 'Q={:3.3f}'.format(qq) ) #S plane pole/zero plot tf 1
 pzmap(my_tf2, fig_id=2, filter_description = 'Q={:3.3f}'.format(qq*2) ) #S plane pole/zero plot tf 2
 
-GroupDelay(my_tf, fig_id=3, filter_description = 'Q={:3.3f}'.format(qq) )
+#retardo de grupo
+GroupDelay(my_tf1, fig_id=3, filter_description = 'Q={:3.3f}'.format(qq) )
 GroupDelay(my_tf2, fig_id=3, filter_description = 'Q={:3.3f}'.format(qq*2) )
 
 
 #%% FORMA ITERATIVA
 
-qq_param = [ np.sqrt(2)/2 , np.sqrt(2) ]
-index = 1;
+qq_param = [ qq1, qq2 ]
+w0_param = [w01,w02]
+index = 1
+
 
 for index in range(len(qq_param)):
     
-    my_tf = TransferFunction( [w0**2], [1, w0/qq_param[index], w0**2] )
+    
+    my_tf = TransferFunction( [w0_param[index]**2], [1, w0_param[index] / qq_param[index], w0_param[index]**2] )
    
 
     bodePlot(my_tf, fig_id=1, filter_description = 'Q={:3.3f}'.format(qq_param[index]) ) #bode tf 1
@@ -54,7 +71,9 @@ for index in range(len(qq_param)):
     GroupDelay(my_tf, fig_id=3, filter_description = 'Q={:3.3f}'.format(qq_param[index]) )
 
     
+#%%
 
+z,p,k=sig.tf2zpk(b, a)
 
 
 
---
+++ tp_semanal_4/segundo_python.py
@@ -0,0 +1,100 @@
+#Pasa altos con butter
+"""
+
+
+@author: juani
+"""
+
+#%% Librerías externas NumPy, SciPy y Matplotlib
+from scipy.signal import TransferFunction     #de el scipy solo exporto la funcion transferfunction
+from scipy import signal as sig  
+import matplotlib.pyplot as plt               #le doy un nombre (plt) al matplotlib para usarlo más comodamente
+import numpy as np                            #le doy un nombre (np) al numpy para usarlo más comodamente
+import math as m
+
+##### Librería de TC2, esta la vas a usar mucho
+from pytc2.sistemas_lineales import pzmap, GroupDelay, bodePlot   #libreria de la catedra. en este caso importo diag. pol. y ceros, retardo y bode
+
+#Defino mi plantilla
+
+alp_MAX=1       #dB
+alp_min=30      #dB
+    
+fs = 10 * 10**3        #Hz
+fp = 40 * 10**3        #Hz
+     
+wp= 2* m.pi *fp
+ws= 2*m.pi*fs
+
+#La normalización en mi pasa altos objetivo
+wp_n_pa=1;             # wp/wp
+ws_n_pa=ws/wp;
+
+#Y la normalización en mi pasa bajos prototipo (con el que voy a trabajar)
+wp_n_pb = 1/wp_n_pa;             
+ws_n_pb = 1/ws_n_pa;
+
+#Se calculan epsilon y n automatico 
+
+eps = m.sqrt( 10**(alp_MAX/10) - 1 )
+    
+n = m.log((m.pow(10, alp_min * 0.1) - 1) / (eps**2))/(2*m.log(ws_n_pb)) 
+n = m.ceil(n)
+
+#Defino mis normas de butter para el prototipo pb y mi pasa altos objetivo
+wb_pb = wp*eps**(-1/n)
+wb_pa = wp*eps**(1/n)
+
+#Obtengo los q (me falta agregarle una forma de automatizar cuantos q sacar en funcion de n)
+qq=1/(2*m.cos(m.pi/n))
+
+
+
+#Defino numerador y den de mis transferencias de 2do y 1er orden 
+
+plt.close('all')        #cierro diagramas pasados
+
+#Primero ploteo mi pasa bajos prototipo
+
+primero_num=[wb_pb**2]
+primero_den=[1, wb_pb/qq , wb_pb**2 ]
+
+segundo_num=[wb_pb]
+segundo_den=[1 , wb_pb]
+
+tf_completa = TransferFunction(np.polymul(primero_num, segundo_num),np.polymul(primero_den,segundo_den))
+
+bodePlot(tf_completa, fig_id=1, filter_description = 'Pasa bajos');
+pzmap(tf_completa, fig_id=2, filter_description = 'Pasa bajos')
+
+#Luego ploteo en el mismo diagrama mi pasa altos
+
+primero_num=[1, 0, 0]
+primero_den=[1, wb_pa/qq , wb_pa**2 ]
+
+segundo_num=[1, 0]
+segundo_den=[1 , wb_pa]
+
+tf_completa = TransferFunction(np.polymul(primero_num, segundo_num),np.polymul(primero_den,segundo_den))
+
+bodePlot(tf_completa, fig_id=1, filter_description = 'Pasa altos');
+pzmap(tf_completa, fig_id=2, filter_description = 'Pasa bajos')
+
+#FIN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
